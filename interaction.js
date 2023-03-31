aad57dbc30f8c8fcd2a7c129b1552bf714b9b54a
kdahlem <dahlem.kilian@gmail.com>
2023-03-31 08:53:29 +0200

    ajout des classes du pattern MVC

--- interaction.js
+++ interaction.js
@@ -4,9 +4,48 @@
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
 
-	// Developper les 3 fonctions gérant les événements
+  this.initX = 0;
+  this.initY = 0;
+  this.finalX = 0;
+  this.finalY = 0;
+  this.isClicked = false;
+  this.interactor = interactor;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+	// Developper les 3 fonctions gérant les événements
+  this.mouseClick = function(evt) {
+    var pos = getMousePosition(canvas, evt);
+    this.initX = pos.x;
+    this.initY = pos.y;
+    this.isClicked = true;
+    //console.log(pos);
+    this.interactor.onInteractionStart(this);
+
+  }.bind(this);
+
+  this.mouseMove = function(evt) {
+    if(this.isClicked){
+      var pos = getMousePosition(canvas, evt);
+      this.finalX = pos.x;
+      this.finalY = pos.y;
+      this.isClicked = true;
+      //console.log(pos);
+      this.interactor.onInteractionUpdate(this);
+    }
+  }.bind(this);
+
+  this.mouseRelease = function(evt) {
+    var pos = getMousePosition(canvas, evt);
+    this.finalX = pos.x;
+    this.finalY = pos.y;
+    this.isClicked = false;
+    //console.log(pos);
+    this.interactor.onInteractionEnd(this);
+  }.bind(this);
+
+  canvas.addEventListener('mousedown', this.mouseClick, false);
+  canvas.addEventListener('mousemove', this.mouseMove, false);
+  canvas.addEventListener('mouseup', this.mouseRelease, false);
+  // Associer les fonctions précédentes aux évènements du canvas.
 };
 
 
--- view.js
+++ view.js
@@ -1,2 +1,71 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+/*
+Rectangle.prototype.paint = function(ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(),   this.getFinalY());
+    ctx.stroke();
+};
+
+Line.prototype.paint = function(ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
+Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function (eltDuTableau) {
+        // now fill the canvas
+        eltDuTableau.paint(ctx);
+    });
+};
+*/
+
+Rectangle.prototype.paint = function(ctx) {
+    ctx.strokeStyle = this.color
+    ctx.lineWidth = this.thickness
+    ctx.strokeRect(this.startX, this.startY, this.width, this.height)
+};
+
+Line.prototype.paint = function(ctx) {
+    ctx.strokeStyle = this.color
+    ctx.lineWidth = this.thickness
+    ctx.beginPath();
+    ctx.moveTo(this.startX, this.startY);
+    ctx.lineTo(this.endX, this.endY);
+    ctx.stroke();
+};
+
+Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.shapeArray.forEach(function (eltDuTableau) {
+        // now fill the canvas
+        eltDuTableau.paint(ctx);
+    });
+};
+
+function updateShapeList(index, shape){
+    document.getElementById('shapeList').insertAdjacentHTML('beforeend', toDom(shape, index))
+}
+
+function toDom(shape, index){
+    if(shape && typeof shape === 'object'){
+        let innerHtml = '<li id="liRemove${index}">'
+        if(shape.constructor === Rectangle){
+            innerHtml += '<span style="color:' + shape.color + '">[]</span> Rectangle'
+        } else if(shape.constructor === Line){
+            innerHtml += '<span style="color:' + shape.color + '">/</span> Line'
+        }
+        innerHtml += '<button type="button" class="btn btn-default remove" id="remove${index}">' +
+            '<span class="glyphicon glyphicon-remove-sign"></span> </button>'
+        return innerHtml + '</li>'
+    }
+}
